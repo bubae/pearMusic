0bb74ee930d9d3be7281ba5b3f7723146cac1dcd
bubae <widianpear@gmail.com>
2016-06-23 14:57:00 +0900

    bbu youtube webview add

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016 Pearman. All rights reserved.
+ * Copyright (c) 2012 The Chromium Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  **/
@@ -10,16 +10,21 @@
  *
  * @see http://developer.chrome.com/apps/app.window.html
  */
- 
 chrome.app.runtime.onLaunched.addListener(function() {
 	var screenWidth = screen.availWidth;
 	var screenHeight = screen.availHeight;
-	chrome.app.window.create('views/pear_music.html', {
-			'outerBounds': {
+	width = 1200;
+	height = 800;
+	chrome.app.window.create('pear_music.html', {
+		id: "pearMusicID",
+		outerBounds: {
 			'width': 1200,
 			'height': 800,
+			minWidth: 764,
+			minHeight: 600,
 			left: Math.round((screenWidth-width)/2),
 			top: Math.round((screenHeight-height)/2)
 		}
 	});
 });
+
--- public/js/view.js
+++ public/js/view.js
@@ -1,20 +1,13 @@
-var app_key = "AIzaSyBjSx3cD_ArNqKRGFGlVzBQl-N7h16EU8Q"
+var app_key = "AIzaSyBjSx3cD_ArNqKRGFGlVzBQl-N7h16EU8Q";
 
 function View(playerModel) {
 	this.searchBox = $('#searchBox');
 	this.searchResult = $('#searchResult');
-	this.searchContent = $('#searchContent')
+	this.searchContent = $('#searchContent');
 	this.buttonsElement = $('#buttons');
 	this.displayElement = $('#display');
 	this.lastDisplayElement = null;
 	var player = this;
-
-
-	var tag = document.createElement('script');
-
-	tag.src = "https://www.youtube.com/iframe_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 	$(document).keydown(function(event) {
 		if (event.keyCode == 27) {
@@ -26,17 +19,17 @@ function View(playerModel) {
 	$("#searchBox").keydown(function(event){
 	    if(event.keyCode == 13){
 	    	if ($("#searchBox")[0].value.length > 1) {
-		        player.videoSearch($("#searchBox")[0].value)
-		        $("#searchBox")[0].value = ""
+		        player.videoSearch($("#searchBox")[0].value);
+		        $("#searchBox")[0].value = "";
 	    	}
 	    }
 	});
 }
 
 View.prototype.videoSearch = function(searchText){
-	template_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q={{searchText}}&key={{app_key}}"
+	template_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q={{searchText}}&key={{app_key}}";
 
-	search_url = template_url.replace("{{searchText}}", searchText).replace("{{app_key}}", app_key)
+	search_url = template_url.replace("{{searchText}}", searchText).replace("{{app_key}}", app_key);
 
 	jQuery.ajax({
 		type: "GET", 
@@ -46,12 +39,12 @@ View.prototype.videoSearch = function(searchText){
 		success: this.searchResultShow,
 		error: errorNoti
 	}); 	
-}
+};
 
 View.prototype.searchResultShow = function(searchData){
 
-	searchContent = $('#searchContent')
-	searchContentTemplate = '<li><a href="#">{{title}}</a></li>'
+	searchContent = $('#searchContent');
+	searchContentTemplate = '<li><a href="#">{{title}}</a></li>';
 	if (searchData == []){
 		this.noSearchResult();
 		return;
@@ -59,20 +52,20 @@ View.prototype.searchResultShow = function(searchData){
 
 	searchContent[0].innerHTML = '';
 	var vidoeInfos = searchData.items;
-	allContent = ""
+	allContent = "";
 	for (i = 0; i < vidoeInfos.length; i++) {
-		title = vidoeInfos[i].snippet.title
-		addItem = searchContentTemplate.replace("{{title}}", title)
+		title = vidoeInfos[i].snippet.title;
+		addItem = searchContentTemplate.replace("{{title}}", title);
 		allContent = allContent + addItem;		
 	}
-	searchContent[0].innerHTML = allContent
+	searchContent[0].innerHTML = allContent;
 	contentEventSetUp();
 	searchContent.show();
-}
+};
 
 function contentEventSetUp(){
 	$("#searchContent li").click(function() {
-		console.log($(this))
+		// console.log($(this));
 	});
 }
 
@@ -81,37 +74,6 @@ function noSearchResult(){
 }
 
 function errorNoti(data) {
-	console.log("Video Search Error")
-	console.log(data)
-}
-
-function onYouTubeIframeAPIReady() {
-	player = new YT.Player('video-player', {
-		height: '240',
-		width: '300',
-		videoId: 'M7lc1UVf-VE',
-		events: {
-			'onReady': onPlayerReady,
-			'onStateChange': onPlayerStateChange
-		}
-	});
-}
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-	event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-	if (event.data == YT.PlayerState.PLAYING && !done) {
-		setTimeout(stopVideo, 6000);
-		done = true;
-	}
-}
-function stopVideo() {
-	player.stopVideo();
+	// console.log("Video Search Error");
+	// console.log(data);
 }
